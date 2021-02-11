d5421d07049cd91122deb794fb36914487c9e6a9
protopopova <protopopova.md@phystech.edu>
2021-02-11 10:49:46 +0300

    факториал рекурсия

---
+++ 0/second_project1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int fact(int i)
+{
+    if (i == 0) return 1;
+    else return i * fact(i - 1);
+}
+
+int main()
+{
+    int n;
+    int i;
+
+    
+    std::cin >> n;
+    int r = fact(n);
+    std::cout << r  << std:: endl;
+}
+
